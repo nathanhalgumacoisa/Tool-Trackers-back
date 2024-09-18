97bc3cbdd98d5f2eae7f5d47b5ec13f64e0bf20c
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-09-18 11:18:04 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3003; 
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Servidor funfando na porta ${port}🛠🔧`);
+});
