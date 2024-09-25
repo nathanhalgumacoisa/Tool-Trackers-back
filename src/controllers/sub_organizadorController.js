e0a07ad44b9eab1e86a820ffe86408f677033224
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-09-25 08:30:21 -0300

    feat(creation of getSub_organizadorId)

--- src/controllers/sub_organizadorController.js
+++ src/controllers/sub_organizadorController.js
@@ -1,14 +1,30 @@
 const pool = require('../config/dbConfig');
 
-async function getAllSub_organizador(req, res) {
+app.get('/sub_organizador', async (req, res) => {
     try {
-        const result = await pool. query('SELECT * FROM sub_organizador');
+        const resultado = await pool.query('SELECT * FROM sub_organizador');
         res.json({
-            total : result.rowCount,
-            sub_organizador : result.rows
+            total: resultado.rowCount,
+            sub_organizador: resultado.rows,
         });
     } catch (error) {
-        console.error('erro ao obter todos os sub organizadores', error);
-        res.json({error: error.message});
+        console.error('Erro ao obter todos os sub organizadores', error);
+        res.status(500).send('Erro ao obter os sub organizadores');
     }
-}
+});
+
+app.get('/sub_organizador/:sub_organizador_id', async(req, res) => {
+    try {
+        const { sub_organizador_id } = req. params;
+        const resultado = await pool.query('SELECT * FROM sub_organizador WHERE sub_organizador_id = $1', [sub_organizador_id])
+        if(resultado.rowCount == 0){
+            res.status(404).send({mensagem: 'Id não encontrado'});
+        }
+        res.json({
+            sub_organizador: resultado.rows[0],
+        })
+    } catch (error) {
+        console.error('Erro ao pegar sub organizador por ID ', error);
+        res.status(500).send('Erro ao pegar sub organizador por ID');
+    }
+});
