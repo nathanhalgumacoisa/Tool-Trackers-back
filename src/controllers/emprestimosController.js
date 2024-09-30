8e7fd1615debff6fb0f325ac62c7473fe2d29722
lia509 <giuliarodrigues@aluno.senai.br>
2024-09-30 09:01:39 -0300

    finalização das routes e controllers

---
+++ src/controllers/emprestimosController.js
@@ -0,0 +1,83 @@
+import pool from "../config/dbConfig.js"
+
+
+export async function getAllEmprestimos (req, res) {
+    try {
+        const result = await pool.query('SELECT * FROM emprestimos;');
+        res.json({
+            total: result.rowCount,
+            emprestimos: result.rows
+        });
+    } catch (error) {
+        console.error('Erro ao pegar emprestimos', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+
+
+export async function getEmprestimosByParam (req, res) {
+    const { param } = req.params;
+    try {
+        let result;
+        if (isNaN(param)) {
+            result = await pool.query('SELECT * FROM emprestimos WHERE categoria LIKE $1;', [`%${param}%`]);
+        } else {
+            result = await pool.query('SELECT * FROM emprestimos WHERE categoria = $1;', [param]);
+        }
+       
+        res.json({
+            total: result.rowCount,
+            emprestimos: result.rows
+        });
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+export async function createEmprestimos (req, res)  {
+    try {
+        const { ferramenta_id, user_id, data_emprestimo, data_retorno, local_origem_id, local_destino_id} = req.body;
+        const result = await pool.query(
+            'INSERT INTO emprestimos (ferramenta_id, user_id, data_emprestimo, data_retorno, local_origem_id, local_destino_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;',
+            [ferramenta_id, user_id, data_emprestimo, data_retorno, local_origem_id, local_destino_id]
+        );
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+
+
+export async function updateEmprestimos (req, res) {
+    const { emprestimo_id  } = req.params;
+    const { ferramenta_id, user_id, data_emprestimo, data_retorno, local_origem_id, local_destino_id } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE emprestimos SET ferramenta_id = $1, user_id = $2, data_emprestimo = $3, data_retorno = $4, local_origem_id = $5, local_destino_id = $6 WHERE emprestimo_id  = $7 RETURNING ;*',
+            [ferramenta_id, user_id, data_emprestimo, data_retorno, local_origem_id, local_destino_id, emprestimo_id ]
+        );
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+export async function deleteEmprestimos (req, res) {
+    const { emprestimo_id } = req.params;
+    try {
+        await pool.query('DELETE FROM emprestimos WHERE emprestimo_id = $1;', [emprestimo_id]);
+        res.json({ message: 'emprestimo deletada com sucesso' });
+    } catch (error) {
+        console.error('Error ao apagar emprestimo', error);
+        res.json({ error: error.message });
+    }
+};
---
+++ src/controllers/log_ferramentasController.js
@@ -0,0 +1,83 @@
+import pool from "../config/dbConfig.js"
+
+
+export async function getAllLog_ferramentas (req, res) {
+    try {
+        const result = await pool.query('SELECT * FROM log_ferramentas;');
+        res.json({
+            total: result.rowCount,
+            log_ferramentas: result.rows
+        });
+    } catch (error) {
+        console.error('Erro ao pegar log_ferramentas', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+
+
+export async function getLog_ferramentasByParam (req, res) {
+    const { param } = req.params;
+    try {
+        let result;
+        if (isNaN(param)) {
+            result = await pool.query('SELECT * FROM log_ferramentas WHERE categoria LIKE $1;', [`%${param}%`]);
+        } else {
+            result = await pool.query('SELECT * FROM log_ferramentas WHERE categoria = $1;', [param]);
+        }
+       
+        res.json({
+            total: result.rowCount,
+            log_ferramentas: result.rows
+        });
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+export async function createLog_ferramentas (req, res)  {
+    try {
+        const { nome, imagem_url, conjunto, numero, patrimonio, modelo, descricao, disponivel, conferido, emprestado, manutencao, localizacao_id, data_atualizacao} = req.body;
+        const result = await pool.query(
+            'INSERT INTO log_ferramentas (nome, imagem_url, conjunto, numero, patrimonio, modelo, descricao, disponivel, conferido, emprestado, manutencao, localizacao_id,  data_atualizacao) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;',
+            [nome, imagem_url, conjunto, numero, patrimonio, modelo, descricao, disponivel, conferido, emprestado, manutencao, localizacao_id,  data_atualizacao]
+        );
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+
+
+export async function updateLog_ferramentas (req, res) {
+    const { ferramenta_id   } = req.params;
+    const { nome, imagem_url, conjunto, numero, patrimonio, modelo, descricao, disponivel, conferido, emprestado, manutencao, localizacao_id, data_atualizacao } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE log_ferramentas SET nome = $1, imagem_url = $2, conjunto = $3, numero = $4, patrimonio = $5, modelo = $6, descricao = $7, disponivel = $8, conferido = $9, emprestado = $10, manutencao = $11, localizacao_id = $12,  data_atualizacao = $13 WHERE ferramenta_id  = $14 RETURNING ;*',
+            [nome, imagem_url, conjunto, numero, patrimonio, modelo, descricao, disponivel, conferido, emprestado, manutencao, localizacao_id, ferramenta_id,  data_atualizacao ]
+        );
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+};
+
+
+export async function deleteLog_ferramentas (req, res) {
+    const { ferramenta_id } = req.params;
+    try {
+        await pool.query('DELETE FROM log_ferramentas WHERE ferramenta_id = $1;', [ferramenta_id]);
+        res.json({ message: 'ferramenta deletada com sucesso' });
+    } catch (error) {
+        console.error('Error ao apagar log_ferramentas', error);
+        res.json({ error: error.message });
+    }
+};
