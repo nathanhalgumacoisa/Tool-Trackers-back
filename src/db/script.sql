82a7a50c68b5d15d74cdacae8d210dae84f7e93e
lia509 <giuliarodrigues@aluno.senai.br>
2024-11-18 15:06:50 -0300

    SCRIPT ARRUMADO PELA VIGÉSIMA VEZZZ

--- src/db/script.sql
+++ src/db/script.sql
@@ -16,7 +16,7 @@ DROP TYPE IF EXISTS tipo_usuario_enum CASCADE;
 
 \c tooltrackers
 
-CREATE TYPE tipo_usuario_enum AS ENUM ('aluno', 'administracao', 'instrutor', 'manutencao');
+CREATE TYPE tipo_usuario_enum AS ENUM ('aluno', 'administracao', 'instrutor');
 
 CREATE TABLE usuarios(
  user_id SERIAL PRIMARY KEY,
@@ -44,13 +44,13 @@ CREATE TABLE sub_organizador(
 );
 
 
-CREATE TYPE ambiente_enum AS ENUM ('oficina mecanica de usinagem', 'oficina eletro eletronica', 'especo maker', 'manutenao');
+CREATE TYPE ambiente_enum AS ENUM ('oficina mecanica de usinagem', 'oficina eletro eletronica', 'especo maker');
 
-CREATE TYPE slug_enum AS ENUM ('ofm', 'oee', 'em', 'manut');
+CREATE TYPE slug_enum AS ENUM ('ofm', 'oee', 'em');
 
 CREATE TABLE localizacoes(
  localizacao_id SERIAL PRIMARY KEY,
- ambiente VARCHAR(20),
+ ambiente VARCHAR(100),
  organizador_id INTEGER,
  slug VARCHAR(20),
  FOREIGN KEY (organizador_id) REFERENCES organizador(organizador_id)
@@ -141,34 +141,33 @@ INSERT INTO usuarios (nome, email, numero_nif, numero_qrcode, tipo_usuario) VALU
 ('Eve','eve@gmail.com', '5678901', '56789012345678901234', 'aluno');
 
 INSERT INTO organizador (nome_organizador, numero_organizador) VALUES
-('Organizador A', 101),
-('Organizador B', 102),
-('Organizador C', 103),
-('Organizador D', 104),
-('Organizador E', 105);
+('carrinhos', 01),
+('armarios', 102),
+('paineis', 00),
+('carrinhos', 104),
+('paineis', 01);
 
 INSERT INTO sub_organizador (organizador_id, nome_suborganizador, numero_suborganizador, foto_url) VALUES
-(1, 'Sub A1', 201,  'http://exemplo.com/ferr1.jpg'),
-(2, 'Sub A2', 202, 'http://exemplo.com/ferr1.jpg'),
-(3, 'Sub B1', 203, 'http://exemplo.com/ferr1.jpg'),
-(4, 'Sub B2', 204, 'http://exemplo.com/ferr1.jpg'),
-(5, 'Sub C1', 205, 'http://exemplo.com/ferr1.jpg');
+(1, 'gaveta', 01,  'http://exemplo.com/ferr1.jpg'),
+(2, 'prateleira', 01, 'http://exemplo.com/ferr1.jpg'),
+(3, 'outros', 00, 'http://exemplo.com/ferr1.jpg'),
+(4, 'gaveta', 01, 'http://exemplo.com/ferr1.jpg'),
+(5, 'outros', 00, 'http://exemplo.com/ferr1.jpg');
 
 
-
-INSERT INTO localizacoes (ambiente, organizador_id) VALUES
-('Sala A', 1),
-('Sala B', 2),
-('Sala C', 3),
-('Sala D', 4),
-('Sala E', 5);
+INSERT INTO localizacoes (ambiente, organizador_id, slug) VALUES
+('oficina mecanica de usinagem', 1, 'ofm'),
+('oficina eletro eletronica', 2, 'oee'),
+('especo maker', 3, 'em'),
+('oficina mecanica de usinagem', 4, 'ofm'),
+('oficina eletro eletronica', 5, 'oee');
 
 INSERT INTO ferramentas (nome, imagem_url, conjunto, numero, patrimonio, modelo, descricao, disponivel, localizacao_id) VALUES
-('Ferramenta 1', 'http://exemplo.com/ferr1.jpg', 'Conjunto A', '001', 'PATR001', 'Modelo 1', 'Descrição 1', TRUE, 1),
-('Ferramenta 2', 'http://exemplo.com/ferr2.jpg', 'Conjunto B', '002', 'PATR002', 'Modelo 2', 'Descrição 2', TRUE, 2),
-('Ferramenta 3', 'http://exemplo.com/ferr3.jpg', 'Conjunto C', '003', 'PATR003', 'Modelo 3', 'Descrição 3', FALSE, 3),
-('Ferramenta 4', 'http://exemplo.com/ferr4.jpg', 'Conjunto D', '004', 'PATR004', 'Modelo 4', 'Descrição 4', TRUE, 4),
-('Ferramenta 5', 'http://exemplo.com/ferr5.jpg', 'Conjunto E', '005', 'PATR005', 'Modelo 5', 'Descrição 5', TRUE, 5);
+('Alicate de Corte', 'https://w7.pngwing.com/pngs/365/40/png-transparent-hand-tool-lineman-s-pliers-knife-diagonal-pliers-plier-technic-handle-nipper-thumbnail.png', 'Conjunto A', '001', 'PATR001', 'Modelo AC-123', 'Alicate ideal para cortes precisos em cabos e fios.', TRUE, 1),
+('Chave de Fenda', 'https://w7.pngwing.com/pngs/990/967/png-transparent-yellow-slothead-screwdriver-screwdriver-tool-screwdriver-element-service-technic-happy-birthday-vector-images-thumbnail.png', 'Conjunto B', '002', 'PATR002', 'Modelo CF-456', 'Chave de fenda com cabo ergonômico e ponta magnética.', TRUE, 2),
+('Martelo', 'https://e7.pngegg.com/pngimages/372/479/png-clipart-framing-hammer-hammer-handle-hammer-thumbnail.png', 'Conjunto C', '003', 'PATR003', 'Modelo M-789', 'Martelo de 500g, ideal para pequenos reparos.', TRUE, 3),
+('Serrote', 'https://acdn.mitiendanube.com/stores/004/052/068/products/c1516cec6467c8809c4a360e8c1aaa94-4cd393841e37b610a817031972021333-1024-1024.png', 'Conjunto D', '004', 'PATR004', 'Modelo S-101', 'Serrote para madeira com lâmina de 50 cm.', TRUE, 4),
+('Furadeira', 'https://www.designi.com.br/images/preview/11324370.jpg', 'Conjunto E', '005', 'PATR005', 'Modelo F-202', 'Furadeira elétrica com função de martelo e velocidade variável.', TRUE, 5);
 
 INSERT INTO emprestimos (ferramenta_id, user_id, data_emprestimo, data_retorno, local_origem_id, local_destino_id) VALUES
 (1, 1, NOW(), NOW() + INTERVAL '7 days', 1, 2),
